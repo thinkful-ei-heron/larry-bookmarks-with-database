8747132d7531084dac9293b45f29714e78cd3277
larry <larryzie@gmail.com>
2019-11-07 01:39:46 -0600

    Added Get /bookmarks endpoint database functionality

--- src/bookmark/bookmark-router.js
+++ src/bookmark/bookmark-router.js
@@ -2,15 +2,21 @@ const express = require('express');
 const uuid = require('uuid/v4');
 const validator = require('validator');
 const logger = require('../logger');
-const { bookmarks } = require('../store');
+const BookmarksService = require('../bookmarks-service');
+const { storeBookmarks } = require('../store');
 
 const bookmarkRouter = express.Router();
 const bodyParser = express.json();
 
 bookmarkRouter
   .route('/bookmarks')
-  .get((req, res) => {
-    res.json(bookmarks);
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db');
+    BookmarksService.getAllBookmarks(knexInstance)
+    .then(bookmarks => {
+      res.json(bookmarks);
+    })
+    .catch(next);
   })
   .post(bodyParser, (req, res) => {
     const { title, rating, url, desc } = req.body;
@@ -67,7 +73,7 @@ bookmarkRouter
       url: url,
       desc: desc 
     }
-    bookmarks.push(bookmark);
+    storeBookmarks.push(bookmark);
     logger.info(`Bookmark with id ${id} created`);
 
     res
@@ -80,7 +86,7 @@ bookmarkRouter
   .route('/bookmarks/:id')
   .get((req, res) => {
     const { id } = req.params;
-    const bookmark = bookmarks.find(b => b.id == id);
+    const bookmark = storeBookmarks.find(b => b.id == id);
     if (!bookmark) {
       logger.error(`Bookmark with id ${id} not found.`);
       return res
@@ -93,14 +99,14 @@ bookmarkRouter
   .delete((req, res) => {
     const { id } = req.params;
 
-    const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
+    const bookmarkIndex = storeBookmarks.findIndex(b => b.id == id);
     if (bookmarkIndex === -1) {
       logger.error(`Bookmark with id ${id} not found.`);
       return res
         .status(404)
         .send('Not found');
     }
-    bookmarks.splice(bookmarkIndex, 1);
+    storeBookmarks.splice(bookmarkIndex, 1);
     logger.info(`Bookmark with id ${id} deleted.`);
 
     res
---
+++ src/bookmarks-service.js
@@ -0,0 +1,31 @@
+const BookmarksService = {
+  getAllBookmarks(knex) {
+    return knex
+             .select('*')
+             .from('bookmarks');
+  },
+/*  insertArticle(knex, newArticle) {
+    return knex
+      .insert(newArticle)
+      .into('blogful_articles')
+      .returning('*')
+      .then(rows => {
+        return rows[0];
+      });
+  },
+  getById(knex, id) {
+    return knex.from('blogful_articles').select('*').where('id', id).first()
+  },
+  deleteArticle(knex, id) {
+    return knex('blogful_articles')
+      .where({ id })
+      .delete()
+  },
+  updateArticle(knex, id, newArticleFields) {
+    return knex('blogful_articles')
+      .where({ id })
+      .update(newArticleFields)
+  },  
+ */}
+
+module.exports = BookmarksService
