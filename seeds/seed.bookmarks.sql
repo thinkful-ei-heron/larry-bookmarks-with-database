f5d5b6ed546645e031f1a35ed7a322301abe8689
larry <larryzie@gmail.com>
2019-11-07 01:05:57 -0600

    Added database migration and seeding

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,5 @@
+INSERT INTO bookmarks (title, url, rating, description)
+VALUES
+  ('Geo Guessr', 'https://www.geoguessr.com', '3', 'Figure out where you are in the world by looking at a street view'),
+  ('A Good Movie To Watch', 'http://agoodmovietowatch.com/', '4', 'Random quality movie selections that are off the beaten path' ),
+  ('YouTube Time Machine', 'http://yttm.tv/', '5', 'Pick a year and watch every available clip from that year on Youtube'  );
